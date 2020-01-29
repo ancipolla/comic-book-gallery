5b35755dbef3276abaf6468f5b622ac5c7683eed
Ali Taylor-Cipolla <alicipolla@outlook.com>
2020-01-28 20:26:16 -0800

    updated comc book model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -15,5 +15,21 @@ namespace ComicBookGallery.Models
         public string DescriptionHtml { get; set; }
         public Creator[] Creators { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText 
+        { get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+        //series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ","-")
+                    .ToLower() + "-" +IssueNumber + ".jpg";
+            }
+        }
     }
 }
